671a075721c782043e0a44a3ec817de6c8561ae3
ljctown-11 <ljctown-11@users.noreply.github.com>
2026-07-03 18:42:03 +0800

    fix: showPasswordModal }} -> }) syntax error crash fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'summer-growth-bank-v11';
+const CACHE_NAME = 'summer-growth-bank-v16';
 const ASSETS = ['manifest.json', 'icon-512.png'];
 
 self.addEventListener('install', event => {
